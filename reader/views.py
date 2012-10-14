8aaaa209ed718e219dcd7a9a0945ff314613db9e
Arnold Krille <arnold@arnoldarts.de>
2012-10-14 22:05:12 +0000

    A bit of layout/gui. And allow to show all posts including the read ones.

--- reader/views.py
+++ reader/views.py
@@ -7,12 +7,14 @@ from django.db.models import Q
 from reader.models import *
 
 class PostListMixin:
-    def get_postlist(self, category=None, feed=None):
+    def get_postlist(self, category=None, feed=None, all=False):
         q = Q()
         if category is not None:
             q = Q(feed__in=Category.objects.get(pk=int(category)).getfeeds())
         if feed is not None:
             q = Q(feed__pk=int(feed))
+        if not all:
+            q &= ~Q(marks__mark__in=['READ'])
         queryset = Posting.objects.filter(q).order_by('-publishdate')
         return queryset
 
@@ -35,6 +37,7 @@ class IndexView(TemplateView, PostListMixin):
         postsperpage = 50
         category = None
         feed = None
+        listall = False
         page = 1
         nextpagelink = '/reader'
         if kwargs.has_key('category'):
@@ -45,11 +48,13 @@ class IndexView(TemplateView, PostListMixin):
             nextpagelink += '/feed/{}'.format(feed)
         if kwargs.has_key('page'):
             page = int(kwargs['page'])
+        if self.request.GET.has_key('all') or kwargs.has_key('all'):
+            listall = True
         previouspagelink = ''
         if page > 1:
             previouspagelink = '{}/page/{}'.format(nextpagelink, page-1)
         nextpagelink += '/page/{}'.format(page+1)
-        postlist = self.get_postlist(category=category, feed=feed)[(page-1)*postsperpage:page*postsperpage]
+        postlist = self.get_postlist(category=category, feed=feed, all=listall)[(page-1)*postsperpage:page*postsperpage]
         #print [ p.displaywide() for p in postlist ]
         return {
                 'pagetitle': 'My feed reader',
